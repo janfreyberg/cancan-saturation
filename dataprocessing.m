e257b89b55ecea52cd28344e08245d888eb32e4a
Jan Freyberg <jan.freyberg@gmail.com>
2017-01-25 13:20:36 +0000

    Changes for server capability
    Adjust file paths
    Include progress bar
    Improve handling of no trials present
    Change data structure (persistent across loop)
    Improve writing to file

--- dataprocessing.m
+++ dataprocessing.m
@@ -19,27 +19,31 @@ trialdur = 10.0;
 discardstart = 0.0;
 
 % provide directory of the data files & naming pattern here
-if exist('C:\EEG Data\cancan', 'dir')
+if exist('C:\EEG Data\cancan\', 'dir')
     filedir = 'C:\EEG Data\cancan';
 elseif exist('/data/group/FANS/cancan/eeg/', 'dir')
     filedir = '/data/group/FANS/cancan/eeg/';
 end
 
-files = dir([filedir, '\*sat*.eeg']);
+files = dir([filedir, '*sat*.eeg']);
 
 % assign group variables to each file
 for isubject = 1:numel(files)
-    idstart = regexp(files(isubject).name, '(\d)(\d)(\d)[abc]');
-    files(isubject).id = files(isubject).name(idstart:(idstart+3));
+    files(isubject).id = regexprep(files(isubject).name, '(_)', '');
+    [idstart, idend] = regexp(files(isubject).id, '(\d+)[ABCabc]');
+    files(isubject).id = files(isubject).id(idstart:idend);
     if strcmp(files(isubject).id(1), '0')
         files(isubject).group = 0;
     end
 end
 
 % Data Analysis Loop
+progressbar; % initialise prog bar
 for isubject = 1:numel(files)
     % clear variables
     clear *_data cfg*
+    % update progress bar
+    progressbar(isubject/numel(files));
     
     % Trial Basics
     cfg_deftrials.dataset = fullfile(filedir, files(isubject).name);
@@ -52,10 +56,19 @@ for isubject = 1:numel(files)
     trl = [];  % initialise to append
     for trigger = triggers
         cfg_deftrials.trialdef.eventvalue = sprintf('T% 3u', trigger);
-        tmp = ft_definetrial(cfg_deftrials);
+        % try statement because sometimes the triggers are fucked
+        try
+            tmp = ft_definetrial(cfg_deftrials);
+        catch
+            continue
+        end
         % Manually add the trigger code
         tmp.trl(:, 4) = trigger;
         trl = vertcat(trl, tmp.trl); %#ok<AGROW>
+    end
+    % build in a check if trl is full up
+    if isempty(trl)
+        continue
     end
     
     
@@ -96,82 +109,84 @@ for isubject = 1:numel(files)
     for i = 1:numel(triggers)
         trigger = triggers(i);
         cfg_fft.trials = trl(:, 4)' == trigger;
-        fft_data(i) = ft_freqanalysis(cfg_fft, prep_data);
+        fft_data(isubject, i) = ft_freqanalysis(cfg_fft, prep_data);
     end
     
     
     % Calculate the signal-to-noise ratio
     for i = 1:numel(triggers)
-        fft_data(i).snrall = [];
-        fft_data(i).snrstimfreq = [];
-        fft_data(i).snrharmonics = [];
-        fft_data(i).stimfreq = stimfreq;  % add the stimfreq
-        fft_data(i).harmonics = 2;  % add the harmonic to analyse
-        fft_data(i).noisebins = 20;
+        fft_data(isubject, i).snrall = [];
+        fft_data(isubject, i).snrstimfreq = [];
+        fft_data(isubject, i).snrharmonics = [];
+        fft_data(isubject, i).stimfreq = stimfreq;  % add the stimfreq
+        fft_data(isubject, i).harmonics = 2;  % add the harmonic to analyse
+        fft_data(isubject, i).noisebins = 20;
         % do the SNR analysis:
-        fft_data(i) = ssvep_calculate_snr(fft_data(i));
-        fft_data(i) = ssvep_calculate_all_snr(fft_data(i));
+        fft_data(isubject, i) = ssvep_calculate_snr(fft_data(isubject, i));
+        fft_data(isubject, i) = ssvep_calculate_all_snr(fft_data(isubject, i));
     end
     
     
     % Identify noisy electrodes
-    noisechannels = false(size(fft_data(1).label));
+    noisechannels = false(size(fft_data(isubject, 1).label));
     % find any electrodes that seem to have been flat
     for i = 1:numel(triggers)
         % anything with crazy high SNRs is a flat channel
         noisechannels = noisechannels | ...
-                        any(fft_data(i).snrall > 200, 2);
+                        any(fft_data(isubject, i).snrall > 200, 2);
         % anything with an SNR of below two is irrelevant
         noisechannels = noisechannels | ...
-                        any(fft_data(i).snrstimfreq < 2, 2);
+                        any(fft_data(isubject, i).snrstimfreq < 2, 2);
     end
     % set those electrodes to NaN in the frequency spectrum
     for i = 1:numel(triggers)
-        fft_data(i).powspctrm(noisechannels, :) = NaN;
-        fft_data(i).snrall(noisechannels, :) = NaN;
-        fft_data(i).snrstimfreq(noisechannels, :) = NaN;
+        fft_data(isubject, i).powspctrm(noisechannels, :) = NaN;
+        fft_data(isubject, i).snrall(noisechannels, :) = NaN;
+        fft_data(isubject, i).snrstimfreq(noisechannels, :) = NaN;
     end
     
     
     % Calculate weighted amplitude
     for i = 1:numel(triggers)
-        fft_data(i).weightedamplitude = [];
-        fft_data(i) = ssvep_combine_harmonic_amplitudes(fft_data(i));
+        fft_data(isubject, i).weightedamplitude = [];
+        fft_data(isubject, i) = ssvep_combine_harmonic_amplitudes(fft_data(isubject, i));
     end
     
-    % find the maximal signal electrode in max stim cond
-    [~, maxelec] = max(mean([fft_data(:).snrstimfreq], 2));
+    % calculate a weighted average by electrode
+    
+    % find the maximal signal electrode in average
+    [~, maxelec] = max(mean([fft_data(isubject, :).snrstimfreq], 2));
     % display the amplitude at those spots
-%     disp([fft_data(1).weightedamplitude(maxelec),...
-%             fft_data(2).weightedamplitude(maxelec),...
-%             fft_data(3).weightedamplitude(maxelec),...
-%             fft_data(4).weightedamplitude(maxelec)]);
+%     disp([fft_data(isubject, 1).weightedamplitude(maxelec),...
+%             fft_data(isubject, 2).weightedamplitude(maxelec),...
+%             fft_data(isubject, 3).weightedamplitude(maxelec),...
+%             fft_data(isubject, 4).weightedamplitude(maxelec)]);
     
     
     % add these values to a large running variable
-    stim16(isubject, 1) = fft_data(1).weightedamplitude(maxelec);
-    stim32(isubject, 1) = fft_data(2).weightedamplitude(maxelec);
-    stim64(isubject, 1) = fft_data(3).weightedamplitude(maxelec);
-    stim100(isubject, 1) = fft_data(4).weightedamplitude(maxelec);
+    stim16(isubject, 1) = fft_data(isubject, 1).weightedamplitude(maxelec);
+    stim32(isubject, 1) = fft_data(isubject, 2).weightedamplitude(maxelec);
+    stim64(isubject, 1) = fft_data(isubject, 3).weightedamplitude(maxelec);
+    stim100(isubject, 1) = fft_data(isubject, 4).weightedamplitude(maxelec);
     
 %     % A quick plot
 %     figure;
 %     title(files(isubject).id);
 %     for i = 1:numel(triggers)
 %         subplot(4, 1, i);
-%         plot(fft_data(i).freq, fft_data(i).snrall(~noisechannels, :));
+%         plot(fft_data(isubject, i).freq, fft_data(isubject, i).snrall(~noisechannels, :));
 %         % ylim([0, 25]);
 %         xlim(cfg_fft.foilim);
 %         % add text to identify channels
-%         text(1.8 + zeros(size(fft_data(1).label)),...
-%              fft_data(i).snrall(:, find(fft_data(i).freq>=1.8, 1)),...
-%              fft_data(i).label);
+%         text(1.8 + zeros(size(fft_data(isubject, 1).label)),...
+%              fft_data(isubject, i).snrall(:, find(fft_data(isubject, i).freq>=1.8, 1)),...
+%              fft_data(isubject, i).label);
 %     end
 % 
 %     % topoplot
 %     figure;
 %     for i = 1:numel(triggers)
-%         tmp_data = fft_data(i);
+%         tmp_data = fft_data(isubject, i);
 %         subplot(2, 2, i);
 %         cfg = [];
 %         cfg.parameter = 'weightedamplitude';
@@ -185,5 +200,7 @@ for isubject = 1:numel(files)
     
 end
 
-allresults = table(stim16, stim32, stim64, stim100, 'Rownames', {files(:).id});
-writetable(allresults, 'allresults.csv', 'Delimiter', ',')
+% write data so far to file
+ids = {files(:).id}';
+allresults = table(ids, stim16, stim32, stim64, stim100);
+writetable(allresults, [date, '-allresults.csv'], 'Delimiter', ',')
