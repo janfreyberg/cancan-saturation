58924ff579015d0f98cc3200d4902dd2319bbd8f
janfreyberg <jan.freyberg@gmail.com>
2017-02-15 10:13:46 +0000

    Re-write EEG analysis to be inside a notebook :sunglasses:

--- preprocessing.m
+++ preprocessing.m
@@ -1,7 +1,28 @@
 
+%% Exported from Jupyter Notebook
+
+%%
+% # EEG Preprocessing Notebook
+% 
+% In this notebook I'll preprocess the data and create new files for resample, reref, etc.
+
+%%
+% ### Fresh start
+% 
+% Clear all and define required variables
+
+%% Cell[1]:
+
 % only run this cell if variables are messed up
 clearvars;
 ft_defaults;
+
+%%
+% ### Setup
+% 
+% The variables required to run this analysis. Always run this cell first, as it also generates the list of files for you!
+
+%% Cell[ ]:
 
 electrodes = 'all';
 stimfreq = 5.0;
@@ -26,6 +47,14 @@ for isubject = 1:numel(files)
     files(isubject).id = files(isubject).id(idstart:idend);
 end
 
+%%
+% ### Data Analysis Step 1: Defining the trials
+% 
+% In this section, the trials are defined.
+
+%% Cell[ ]:
+
+%%capture
 for isubject = 1:numel(files)
     clear prep_data cfg*
     
@@ -55,6 +84,14 @@ for isubject = 1:numel(files)
 
 end
 
+%%
+% ### Data Analysis Step 2: Resampling the data
+% 
+% In this section the data gets "preprocessed" only to the extent that it's snipped to the right length and resampled. In addition, a 50Hz filter and an extreme lowpass filter is applied.
+
+%% Cell[ ]:
+
+%%capture
 for isubject = 1:numel(files)
     % Skip participant if no trials defined
     if isempty(trl{isubject})
@@ -62,26 +99,55 @@ for isubject = 1:numel(files)
     end
     
     % Define the config
-    cfg = [];
-    cfg.dataset = fullfile(filedir, files(isubject).name);
-    cfg.trl = trl{isubject}; % previously defined
-    cfg.channel = electrodes; % defined above
-    cfg.demean = 'yes';  % Subtract mean within each trial
-    cfg.detrend = 'yes';  % equivalent to hi-pass filter
-    cfg.reref = 'no'; % done in step 3
-    cfg.lpfilter = 'yes'; % low pass filter
-    cfg.lpfreq = 25;
+    cfg{isubject} = [];
+    % I/O
+    cfg{isubject}.dataset = fullfile(filedir, files(isubject).name);
+    cfg{isubject}.outputfile = [fullfile(filedir, files(isubject).name), '.resampled.mat'];
+    % Details of the trial
+    cfg{isubject}.trl = trl{isubject}; % previously defined
+    cfg{isubject}.channel = electrodes; % defined above
+    cfg{isubject}.demean = 'yes';  % Subtract mean within each trial
+    cfg{isubject}.detrend = 'yes';  % equivalent to hi-pass filter
+    cfg{isubject}.reref = 'no'; % done in step 3
+    cfg{isubject}.lpfilter = 'yes'; % low pass filter
+    cfg{isubject}.lpfreq = 25;
     
-    prep_data = ft_preprocessing(cfg);
-    
-    cfg = [];
-    cfg.resamplefs = 250;
-    cfg.detrend = 'no';
-    cfg.outputfile = [fullfile(filedir, files(isubject).name), '.resampled.mat'];
-    
-    ft_resampledata(cfg, prep_data);
 end
 
+% Remove the empty cells so matlab doesn't have a stroke
+cfg(cellfun(@isempty, cfg)) = [];
+% Preprocess (in parallel)
+peercellfun(@ft_preprocessing, cfg);
+
+% Resample the data
+cfg = [];
+for isubject = 1:numel(files)
+    
+    if ~exist([fullfile(filedir, files(isubject).name), '.resampled.mat'], 'file')
+        continue
+    end
+    
+    
+    cfg{isubject}.resamplefs = 250;
+    cfg{isubject}.detrend = 'no';
+    cfg{isubject}.inputfile = [fullfile(filedir, files(isubject).name), '.resampled.mat'];
+    cfg{isubject}.outputfile = [fullfile(filedir, files(isubject).name), '.resampled.mat'];
+end
+
+% Remove the empty cells so matlab doesn't have a stroke
+cfg(cellfun(@isempty, cfg)) = [];
+% Resample (in parallel)
+peercellfun(@ft_resampledata, cfg);
+
+
+%%
+% ### Preprocessing Step 3: Doing Preliminary frequency analysis
+% 
+% Here we do a preliminary FFT analysis, calculate the power in the frequency bands, and then class electrodes as noisy depending on their power spectrum.
+
+%% Cell[ ]:
+
+%%capture
 for isubject = 1:numel(files)
     % Skip participant if no data exists so far
     if ~exist([fullfile(filedir, files(isubject).name), '.resampled.mat'], 'file')
@@ -98,101 +164,133 @@ for isubject = 1:numel(files)
     cfg.channel = electrodes;
     cfg.keeptrials = 'no';
     
+    % Identify Noisy Channels
+    noisechannels{isubject} = false([1, 32]);
     for i = 1:numel(triggers)
         % FFT analysis
         trigger = triggers(i);
         cfg.trials = trl{isubject}(:, 4)' == trigger;
-        fft_data{isubject, i} = ft_freqanalysis(cfg);
+        freq = ft_freqanalysis(cfg);
         
         % Extra SNR analysis
-        fft_data{isubject, i}.stimfreq = stimfreq;  % add the stimfreq
-        fft_data{isubject, i}.harmonics = 2;  % add the harmonic to analyse
-        fft_data{isubject, i}.noisebins = 20;
+        freq.stimfreq = stimfreq;  % add the stimfreq
+        freq.harmonics = 2;  % add the harmonic to analyse
+        freq.noisebins = 20;
 
-        snr_data{isubject, i} = ssvep_calculate_snr(fft_data{isubject, i});
-    end
-    
-    
-    
-    
-end
-
-for isubject = 1:numel(files)
-    % Skip participant if no fft data
-    if isempty([fft_data{isubject, :}])
-        continue
-    end
-
-    noisechannels{isubject} = false(size(fft_data{isubject, i}.label));
-    
-    for i = 1:numel(triggers)
+        snr_data = ssvep_calculate_snr(freq);
+        snrspectrum = squeeze(permute(ssvep_calculate_all_snr(freq), [2, 3, 1]));
+        
         % anything with crazy high SNRs is a flat channel
         noisechannels{isubject} = noisechannels{isubject} | ...
-                        any(snr_data{isubject, i}.snrall > 200, 2);
+                        any(snrspectrum > 200, 2);
         % any channels with too much or too little power in the noise band is removed
         noisechannels{isubject} = noisechannels{isubject} | ...
-                        snr_data{isubject, i}.noisepow > 15 | snr_data{isubject, i}.noisepow <= 0;
+                        snr_data.noisepow > 15 | snr_data.noisepow <= 0;
     end
 end
 
+clear freq, snr_data, snrspectrum
+
+%%
+% ### Preprocessing Step 5: Rereferencing
+% 
+% Now that we know where the noisy electrodes are, we can rereference without them.
+
+%% Cell[ ]:
+
+%%capture
+assert(numel(noisechannels)==numel(files), 'did you run the prev cell for noisechannels?')
+cfg = [];
 for isubject = 1:numel(files)
     % Skip participant if no noisechannel data (was not processed in prev. cell)
     if isempty(noisechannels{isubject})
         continue
     end
-    
-    cfg = [];
-    cfg.inputfile = [fullfile(filedir, files(isubject).name), '.resampled.mat'];
-    cfg.outputfile = [fullfile(filedir, files(isubject).name), '.rereferenced.mat'];
-    cfg.reref = 'yes';
-    cfg.refchannel = fft_data{isubject, i}.label(~noisechannels{isubject});
-    if numel(fft_data{isubject, i}.label) == 64; cfg.channel = 33:64; end
-    
-    ft_preprocessing(cfg);
+
+    cfg{isubject}.inputfile = [fullfile(filedir, files(isubject).name), '.resampled.mat'];
+    cfg{isubject}.outputfile = [fullfile(filedir, files(isubject).name), '.rereferenced.mat'];
+    cfg{isubject}.reref = 'yes';
+    cfg{isubject}.refchannel = fft_data{isubject, i}.label(~noisechannels{isubject});
+    if numel(fft_data{isubject, i}.label) == 64; cfg{isubject}.channel = 33:64; end
+
 end
 
+% Remove the empty cells so matlab doesn't have a stroke
+cfg(cellfun(@isempty, cfg)) = [];
+% Do the preprocessing (parallel)
+peercellfun(@ft_preprocessing, cfg);
 
+%%
+% ### Time / Frequency Analysis Step 1: Fourier Transfer
+% 
+% If you've already pre-processed data above (i.e. there is a file called `*rereferenced.mat` for every of your participants) you can jump straight in here.
+
+%% Cell[ ]:
+
+%%capture
+cfg = [];
 for isubject = 1:numel(files)
     % Skip participant if no data exists (probably no triggers)
     if ~exist([fullfile(filedir, files(isubject).name), '.rereferenced.mat'], 'file'), continue, end
     
-    % FFT analysis
-    cfg = [];
-    cfg.inputfile = [fullfile(filedir, files(isubject).name), '.rereferenced.mat'];
-    % cfg.outputfile = [fullfile(filedir, files(isubject).name), '.fft.mat'];
-    cfg.output = 'pow';
-    cfg.method = 'mtmfft';
-    cfg.foilim = [1, 20];
-    cfg.tapsmofrq = 1/(trialdur - discardstart);
-    cfg.channel = electrodes;
-    cfg.keeptrials = 'no';
-    
     for i = 1:numel(triggers)
-        % subset the trials
-        cfg.trials = trl{isubject}(:, 4)' == triggers(i);
-        % do the TFR
-        fftbytrigger{i} = ft_freqanalysis(cfg);
+        cfg{isubject, i}.inputfile = [fullfile(filedir, files(isubject).name), '.rereferenced.mat'];
+        cfg{isubject, i}.outputfile = [fullfile(filedir, files(isubject).name), '.fft_', num2str(i), '.mat'];
+        cfg{isubject, i}.output = 'pow';
+        cfg{isubject, i}.method = 'mtmfft';
+        cfg{isubject, i}.foilim = [1, 20];
+        cfg{isubject, i}.tapsmofrq = 1/(trialdur - discardstart);
+        cfg{isubject, i}.channel = electrodes;
+        cfg{isubject, i}.keeptrials = 'no';
+        cfg{isubject, i}.trials = trl{isubject}(:, 4)' == triggers(i);
     end
-    
-    % combine the four analyses
-    % NB this changes the dimensions of the data, which I've tried to make my data recognise
-    cfg = [];
-    cfg.outputfile = [fullfile(filedir, files(isubject).name), '.fft.mat'];
-    cfg.appenddim = 'rpt';
-    cfg.parameter = 'powspctrm';
-    
-    ft_appendfreq(cfg, fftbytrigger{:});
 end
 
-clear fftbytrigger
+% Remove any empty cells from cfg
+cfg(cellfun(@isempty, cfg)) = [];
+% Do the Freq analysis (in parallel)
+peercellfun(@ft_freqanalysis, cfg);
+
+
+% Append the data from the four separate freq analyses
+cfg = [];
+for isubject = 1:numel(files)
+    % Skip participant if no data exists (probably no triggers)
+    if ~exist([fullfile(filedir, files(isubject).name), '.rereferenced.mat'], 'file'), continue, end
+    
+    cfg{isubject}.outputfile = [fullfile(filedir, files(isubject).name), '.fft.mat'];
+    cfg{isubject}.appenddim = 'rpt';
+    cfg{isubject}.parameter = 'powspctrm';
+    % Add the four separate input files created above
+    for i = 1:numel(triggers)
+        cfg{isubject}.inputfile{i} = [fullfile(filedir, files(isubject).name), '.fft_', num2str(i), '.mat'];
+    end
+end
+
+peercellfun(@ft_appendfreq, cfg);
+
+% Delete the 4 individual files we had previously
+for isubject = 1:numel(files)
+    for i = 1:numel(triggers)
+        
+    end
+end
+
+
+%%
+% ### Time / Frequency analysis Step 2: Signal to noise ratios
+% 
+% 
+
+%% Cell[ ]:
 
 for isubject = 1:numel(files)
     
-    if ~exist([fullfile(filedir, files(isubject).name), '.fft.mat'], 'file'), continue, end
-    
-    fftdata = load([fullfile(filedir, files(isubject).name), '.fft.mat']);
-    freq = fftdata.freq;
-    clear fftdata;
+    if ~exist([fullfile(filedir, files(isubject).name), '.fft.mat'], 'file')
+        continue
+    else
+        freq = getfield(load([fullfile(filedir, files(isubject).name), '.fft.mat']), 'freq');
+    end
     
     freq.stimfreq = stimfreq;
     freq.harmonics = 2;
@@ -213,6 +311,14 @@ for isubject = 1:numel(files)
     save([fullfile(filedir, files(isubject).name), '.fft.mat'], 'freq');
 end
 
+%%
+% ### Time / Frequency analysis Step 3: Combine the harmonics and electrodes
+% 
+% This uses the SNRs calculated earlier to work out the amplitude of the oscillations.
+
+%% Cell[ ]:
+
+%%capture
 for isubject = 1:numel(files)
     
     if ~exist([fullfile(filedir, files(isubject).name), '.fft.mat'], 'file')
@@ -234,6 +340,14 @@ for isubject = 1:numel(files)
     [~, maxelec] = max(mean(freq.snr.snrstimfreq(:, :), 1), [], 2);
 end
 
+%%
+% ### Create a Results Table
+% 
+% The results from the previous three steps can be written to a CSV file in this loop.
+
+%% Cell[ ]:
+
+%%capture
 data_maxelec = NaN([numel(files), numel(triggers)]);
 data_allelec = NaN([numel(files), numel(triggers)]);
 for isubject = 1:numel(files)
@@ -259,8 +373,23 @@ for i = 1:numel(triggers)
     allresults.(['allelec_', num2str(triggers(i))]) = data_allelec(:, i);
 end
 
+%%
+% ### Write to file
+% 
+% Save the results table as a CSV file.
+
+%% Cell[ ]:
+
 writetable(allresults, [date, '-allresults.csv'], 'Delimiter', ',');
 
+%%
+% ### Final Analysis Step 1: Grand Average
+% 
+% 
+
+%% Cell[ ]:
+
+%%capture
 cfg = [];
 cfg.keepindividual = 'no';
 cfg.parameter = {'powspctrm', 'weightedamplitude', 'snrspectrum'};
@@ -279,8 +408,19 @@ for i = 1:numel(triggers)
         fftdata{isubject} = ft_selectdata(cfg, fftdata{isubject});
     end
     cfg.outputfile = fullfile(filedir, ['saturation_grandav_', num2str(triggers(i)), '.mat']);
+    
+    % remove empty cells (people who didn't have triggers)
+    fftdata(cellfun('isempty', fftdata)) = [];
+    % do the grand average
     ft_freqgrandaverage(cfg, fftdata{:});
 end
+
+%%
+% ### Final Analysis Step 2: Topoplot of SNR
+% 
+% Check where the SNR is distributed
+
+%% Cell[ ]:
 
 %plot -s 800,800
 
